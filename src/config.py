ed2b4b55d16742d3abb20e96fdc113cf913cfffb
melhaj7 <ichibakry7@gmail.com>
2026-04-21 12:47:39 +0000

    feat: add silver layer logic

--- src/config.py
+++ src/config.py
@@ -5,3 +5,8 @@ BRONZE_ARRIVALS    = "tfl_pipeline.bronze.tfl_raw_arrivals"
 BRONZE_DISRUPTIONS = "tfl_pipeline.bronze.tfl_raw_disruptions"
 BRONZE_STOP_POINTS = "tfl_pipeline.bronze.tfl_raw_stop_points"
 BRONZE_TIMETABLES  = "tfl_pipeline.bronze.tfl_raw_timetables"
+
+# Silver table names
+SILVER_ARRIVALS    = "tfl_pipeline.silver.tfl_arrivals"
+SILVER_DISRUPTIONS = "tfl_pipeline.silver.tfl_disruptions"
+SILVER_STOP_POINTS = "tfl_pipeline.silver.tfl_stop_points"
